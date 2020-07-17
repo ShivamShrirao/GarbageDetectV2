62034be2f4cdb034a34395d4d14c12c3623fa96c
ShivamShrirao <shivamshrirao@gmail.com>
2020-07-18 01:30:10 +0530

    convert labels done

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ IMG_SIZE = 20
 
 path = "/home/archer/machine_learning/garbage_detection/garbage_dataset/"
 garbage = "garbage"
-filename,width,height,label,xmin,ymin,xmax,ymax = 'b6b92d2a-679e-11e5-b0d3-40f2e96c8ad8.jpg',1600,1131,'garbage',688.5714285714284,442.8571428571428,1374.2857142857142,638.5714285714284
+filename,width,height,label,xmin,ymin,xmax,ymax = 'ba01cf5a-679e-11e5-b0d3-40f2e96c8ad8.jpg',636,546,garbage,103.0,306.0,612.0,527.0
 
 img = cv2.imread(path+filename)								# No need to read image btw.
+plt.imshow(img)
+plt.show()
 
 wid_r = IMG_SIZE/width
 hgt_r = IMG_SIZE/height
@@ -30,28 +32,28 @@ len_grid = IMG_SIZE//20
 
 # img = cv2.rectangle(img, (a_xmin,a_ymin), (a_xmax,a_ymax), (255, 0, 0), 1)
 
-arr = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+arr = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY).astype(np.float32)
 
-arr = np.asarray(arr, np.float32)
 arr[a_ymin:a_ymax,a_xmin:a_xmax][:] = -1
-img[a_ymin:a_ymax,a_xmin:a_xmax][:] = -1
 
 OUT_SIZE = 20
-area_thresh = OUT_SIZE*OUT_SIZE * 0.5
+# area_thresh = OUT_SIZE*OUT_SIZE * 0.5
 
-label = np.zeros((OUT_SIZE, OUT_SIZE), dtype=np.bool)
+# label = np.zeros((OUT_SIZE, OUT_SIZE), dtype=np.bool)
 
-idy = 0
-for y in range(0, IMG_SIZE, len_grid):
-	idx = 0
-	for x in range(0, IMG_SIZE, len_grid):
-		iou = (arr[y:y+len_grid, x:x+len_grid] == -1).sum()
-		if iou >= area_thresh:
-			label[idy,idx] = True
-		elif iou > 150:
-			print(idy, idx, iou)
-		idx+=1
-	idy+=1
+label = (arr == -1)
+
+# idy = 0
+# for y in range(0, IMG_SIZE, len_grid):
+# 	idx = 0
+# 	for x in range(0, IMG_SIZE, len_grid):
+# 		iou = (arr[y:y+len_grid, x:x+len_grid] == -1).sum()
+# 		if iou >= area_thresh:
+# 			label[idy,idx] = True
+# 		elif iou > 150:
+# 			print(idy, idx, iou)
+# 		idx+=1
+# 	idy+=1
 
 plt.imshow(img)
 plt.imshow(cv2.resize(label.astype(np.uint8), (IMG_SIZE, IMG_SIZE)), alpha=0.3)
