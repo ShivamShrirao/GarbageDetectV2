ab98f8d2f3dcdd551ef5a20003fcc7a5fb82c37e
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-01 23:48:03 +0530

    new trained 96.5%

--- heartbeat.py
+++ heartbeat.py
@@ -3,7 +3,7 @@ import requests
 
 API_ENDPOINT = "http://annotate.ret2rop.com:31896/monitoring/"
 uid = "DEVICE 1 ID"
-data = {"uid": uid}
+data = {"uid": uid, "auth-toekn": "ENTER_AUTH_TOKEN"}
 
 
 while True:
