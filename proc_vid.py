5c950156beb3e336c9d5b629077e1df422b52e57
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-02 00:25:21 +0530

    min area rect

--- proc_vid.py
+++ proc_vid.py
@@ -6,15 +6,15 @@ from time import time
 IMG_HEIGHT = 320
 IMG_WIDTH = 320
 
-tflite_path = 'new_model_0_75/my_model_fp32.tflite'
+tflite_path = 'new_model_0_75_-1to1/my_model_fp32_320x320.tflite'
 tflite_interpreter = tf.lite.Interpreter(model_path=tflite_path)
 tflite_interpreter.allocate_tensors()
 tflite_input_details = tflite_interpreter.get_input_details()
 tflite_output_details = tflite_interpreter.get_output_details()
 
-cam = cv2.VideoCapture('../garbage_detection/vid2.mp4')
+cam = cv2.VideoCapture('../garbage_detection/vid.mp4')
 fps = 40#round(cam.get(cv2.CAP_PROP_FPS))
-writer = cv2.VideoWriter('segout.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fps,(IMG_WIDTH,IMG_HEIGHT))
+writer = cv2.VideoWriter('segout_bb.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fps,(IMG_WIDTH,IMG_HEIGHT))
 
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("output", 600,600)
@@ -31,7 +31,7 @@ while cam.isOpened():
 	oimg = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))
 #     cv2.imshow("output",oimg)
 	img = cv2.cvtColor(oimg, cv2.COLOR_BGR2RGB).astype(np.float32)
-	img/= 255
+	img = img/127.5 - 1
 	tflite_interpreter.set_tensor(tflite_input_details[0]['index'], np.expand_dims(img, axis=0))
 	tflite_interpreter.invoke()
 	pred = tflite_interpreter.get_tensor(tflite_output_details[0]['index']).squeeze()
@@ -48,15 +48,20 @@ while cam.isOpened():
 	cv2.putText(heated, str(fps)[:5]+" FPS", (0, 15),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 1)
 	contours,_ = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	if len(contours) > 0:
-	    c = max(contours,key=cv2.contourArea)
-	    if cv2.contourArea(c)>100:
-	        #Getting the bounding rectangle
-	        x,y,w,h = cv2.boundingRect(c)
-	        #Drawing the bounding rectangle
-	        cv2.rectangle(heated,(x,y),(x+w,y+h),(0,255,0),2)
-	        #Getting the moments
-	        m = cv2.moments(c)
-	        #moving mouse to the centroid
+		c = max(contours,key=cv2.contourArea)
+		if cv2.contourArea(c)>100:
+			#Getting the bounding rectangle
+			# x,y,w,h = cv2.boundingRect(c)
+			#Drawing the bounding rectangle
+			# cv2.rectangle(heated,(x,y),(x+w,y+h),(0,255,0),2)
+			# min rect
+			rect = cv2.minAreaRect(c)
+			box = cv2.boxPoints(rect)
+			box = np.int0(box)
+			cv2.drawContours(heated,[box],0,(0,255,0),2)
+			#Getting the moments
+			# m = cv2.moments(c)
+			#moving mouse to the centroid
 	# # cv2.drawContours(heated,contours,-1,(255,0,0),2)
 	# cv2.imshow("output",heated)
 	cv2.imshow("output", heated)
